4d2ea054c49ff5842c0d149b3a67420772d4bc05
Raymond Tay <raymondtay1974@gmail.com>
2020-02-15 12:24:51 +0800

    Foldable typeclass

--- src/main/scala/parallelism/FoldMap.scala
+++ src/main/scala/parallelism/FoldMap.scala
@@ -8,10 +8,32 @@ package parallel
  */
 trait FoldMap {
 
-  // Traverses the data structure and for each element, applies the function
   // finally collects and collapses the result.
   def foldMap[A,B : Monoid](xs: List[A])(f: A => B) : B = 
     xs.map(f).foldLeft(Monoid[B].mzero)(_ |+| _)
 
+  def foldMapF[A,B : Monoid, F[_] : Functor : Foldable](xs: F[A])(f: A => B) : B = {
+    val fb = Functor[F].map(xs)(f)
+    Foldable[F].fold(fb)(Monoid[B].mzero)(_ |+| _)
+  }
+
 }
 
+object MapReduce extends App with FoldMap {
+
+  import FoldableInstances._
+
+  // I have only defined how to combine values of the String type,
+  // so it follows the demonstration is as follows.
+  println(foldMap(List("1", "2", "3", "4", "5"))(_ + "!"))
+
+  // Question: I have defined how to handle values of the element in a List, or
+  // more generally i have defined how to handle values of the elements in a
+  // structure (You should be convinced that List ∈ Structure) ; next question
+  // to ask is whether we can generalize it to include the "Structure" in which
+  // we apply to ?
+  // Answer : Yes.
+
+  println(foldMapF(List("1", "2", "3", "4", "5"))(_ + "!"))
+
+}
--- src/main/scala/parallelism/package.scala
+++ src/main/scala/parallelism/package.scala
@@ -30,5 +30,29 @@ package object parallel {
     def apply[A](implicit m : Monoid[A]) : Monoid[A] = m
   }
 
+  // Foldable is a behavior where i wish to represent the notion of folding a
+  // structure with the end-goal of collapsing the entire structure to a single
+  // value. However, i would need to extend from the Functor typeclass
+  // (typeclass == behaviors) as it provides the basic notion of traversing a
+  // structure (that is what "map" is for).
+  trait Foldable[F[_]] extends Functor[F] {
+    def fold[A](fa: F[A])(z: A)(f : (A,A) => A) : A
+  }
+
+  object Foldable {
+    def apply[F[_]](implicit f : Foldable[F]) : Foldable[F] = f
+  } 
+
+  object FoldableInstances {
+
+    // here is where i define what it means to fold a List of As
+    // but take note that `Foldable` represents a structure.
+    implicit val FL : Foldable[List] = new Foldable[List] {
+      def fold[A](fa: List[A])(z:A)(f : (A, A) => A) : A = fa.fold(z)(f)
+      def map[A,B](fa: List[A])(f: A => B) : List[B] = fa.map(f)
+    }
+
+  }
+
 }
 
